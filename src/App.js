b91960ea909c95a544e2bb6c9e83809b17612519
Piepognwong <j.tonneyck@gmail.com>
2020-03-31 12:29:36 +0200

    added conditionals

--- src/App.js
+++ src/App.js
@@ -18,23 +18,41 @@ class App extends Component {
   };
 
   filterStudents = (e)=> {
+    debugger
     let filterTheStudents = this.state.students.filter((student)=> {
       return(
       student.firstname.toLowerCase().includes(e.target.value.toLowerCase()) ||
       student.lastname.toLowerCase().includes(e.target.value.toLowerCase()) 
     )});
-
+    debugger
     this.setState({filteredStudents: filterTheStudents});
   }
   render() {
+    debugger
+    let biggerThan;
+    if(this.state.counter > 5){
+      biggerThan = <h1>Bigger than 5!</h1>
+    } else {
+      biggerThan =  <h1>Not bigger than 5</h1>
+    }
     return (
       <div className="App">
         <h1>Count {this.state.counter}</h1>
         <button onClick={this.increment}>increment</button>   
+        {this.state.counter > 5? 
+          <h1>Bigger than 5!</h1>:
+          <h1>Not bigger than 5</h1>
+        }
+        
+
+        {biggerThan}
+        {this.state.counter > 10 && <h1>Bigger than 10!</h1>}
+        
         <h1>Search Students</h1>
         <input type="text" onChange={this.filterStudents}/>
-        {this.state.filteredStudents.map((student)=> (
+        {this.state.filteredStudents.map((student, index)=> (
           <Student 
+            key={`${student.firstname}-${index}`}
             firstname={student.firstname} 
             lastname={student.lastname}
             image={`https://wd-ft-feb.s3.eu-central-1.amazonaws.com/${student.firstname.toLowerCase()}.png`}

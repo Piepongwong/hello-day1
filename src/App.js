e779b439f5e9b0e42a5cbd7cee29a0858308211e
Piepognwong <j.tonneyck@gmail.com>
2020-03-31 09:32:49 +0200

    added counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Student from "./Student";
-import Bootcamp from "./Bootcamp";
+import Student from "./components/Student";
+import Bootcamp from "./components/Bootcamp";
 import students from "./students.json";
 // https://wd-ft-feb.s3.eu-central-1.amazonaws.com/alena.pn
 class App extends Component {
+  state = {
+    counter: 0
+  };
+
+  increment = ()=>{
+    this.setState({counter: this.state.counter + 1});
+  };
+
   render() {
     return (
       <div className="App">
+        <h1>Count {this.state.counter}</h1>
+        <button onClick={this.increment}>increment</button>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Ironhack</h2>
